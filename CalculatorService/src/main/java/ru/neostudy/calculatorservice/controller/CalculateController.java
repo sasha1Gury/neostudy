81d4f0be31a157ceb7649629e6733095cc7a2853
sasha1Gury <sashagury456123@mail.ru>
2024-03-31 22:18:38 +0300

    feat: add interface CalculateService

--- CalculatorService/src/main/java/ru/neostudy/calculatorservice/controller/CalculateController.java
+++ CalculatorService/src/main/java/ru/neostudy/calculatorservice/controller/CalculateController.java
@@ -1,12 +1,12 @@
 package ru.neostudy.calculatorservice.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.neostudy.calculatorservice.dto.RequestDateDto;
 import ru.neostudy.calculatorservice.dto.RequestDto;
 import ru.neostudy.calculatorservice.service.CalculateService;
+import ru.neostudy.calculatorservice.service.CalculateServiceImpl;
 
 @RestController
 //@RequiredArgsConstructor
--- CalculatorService/src/main/java/ru/neostudy/calculatorservice/service/CalculateService.java
+++ CalculatorService/src/main/java/ru/neostudy/calculatorservice/service/CalculateService.java
@@ -1,65 +1,10 @@
 package ru.neostudy.calculatorservice.service;
 
-import org.springframework.stereotype.Service;
 import ru.neostudy.calculatorservice.dto.RequestDateDto;
 import ru.neostudy.calculatorservice.dto.RequestDto;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
+public interface CalculateService {
+    Double calculate(RequestDto requestDto);
 
-@Service
-public class CalculateService {
-    private final List<LocalDate> holidayList = List.of(
-            LocalDate.of(2024, 1, 1),
-            LocalDate.of(2024, 1, 2),
-            LocalDate.of(2024, 1, 3),
-            LocalDate.of(2024, 1, 4),
-            LocalDate.of(2024, 1, 5),
-            LocalDate.of(2024, 1, 6),  // Новый год
-            LocalDate.of(2024, 1, 8),  // Рождество
-            LocalDate.of(2024, 2, 23), // День защитника Отечества
-            LocalDate.of(2024, 3, 8),  // Международный женский день
-            LocalDate.of(2024, 5, 1),
-            LocalDate.of(2024, 5, 2),
-            LocalDate.of(2024, 5, 3),  // Праздник Весны и Труда
-            LocalDate.of(2024, 5, 9),  // День Победы
-            LocalDate.of(2024, 6, 12), // День России
-            LocalDate.of(2024, 11, 4)  // День народного единства
-    );
-
-    public Double calculate(RequestDto requestDto) {
-        Double daysInMonth = 29.4;
-        Double avgMonthSalary = requestDto.getAvgSalary() / 12;
-        Double avgDaysSalary = avgMonthSalary / daysInMonth;
-        return avgDaysSalary * requestDto.getVacationDays();
-    }
-
-    public Double calculateWithDate(RequestDateDto requestDateDto) {
-        long vacationDays = calculateDate(requestDateDto.getBeginVacation(), requestDateDto.getEndVacation());
-        Double daysInMonth = 29.4;
-        Double avgMonthSalary = requestDateDto.getAvgSalary() / 12;
-        double avgDaysSalary = avgMonthSalary / daysInMonth;
-        System.out.println(vacationDays);
-        return avgDaysSalary * vacationDays;
-    }
-
-    private Long calculateDate(LocalDate beginVacation, LocalDate endVacation) {
-        long daysBetween = ChronoUnit.DAYS.between(beginVacation, endVacation) + 1; //не учитывается 1 день
-        LocalDate currentDate = beginVacation;
-
-        while (!currentDate.isAfter(endVacation)) {
-
-            if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY ||
-                    currentDate.getDayOfWeek() == DayOfWeek.SUNDAY ||
-                    holidayList.contains(currentDate)) {
-                daysBetween--;
-            }
-
-            currentDate = currentDate.plusDays(1);
-        }
-        return daysBetween;
-    }
-
+    Double calculateWithDate(RequestDateDto requestDateDto);
 }
